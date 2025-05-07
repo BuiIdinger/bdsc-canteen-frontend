9f76d1a490f01f29be319f3cc2be9c1700255023
Shayden seymour <shaydenseymour@gmail.com>
2025-05-08 08:50:10 +1200

    Menu implemented

---
+++ components/MenuProduct.vue
@@ -0,0 +1,41 @@
+<template>
+  <NuxtLink
+    class="h-[186px] w-[186px]"
+    :to="to"
+  >
+    <!-- Product image -->
+    <div class="h-full w-full bg-gray-300">
+      <NuxtImg
+        width="full"
+        height="full"
+        :src="image"
+      >
+
+      </NuxtImg>
+    </div>
+    <!-- Product name-->
+    <div class="mt-[10px]">
+      <p>{{ name }}</p>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue"
+
+defineProps({
+  to: {
+    type: String,
+    required: true,
+    default: "\n"
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  }
+});
+</script>
--- pages/menu/index.vue
+++ pages/menu/index.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>meun</h1>
+  <!-- Title & Description -->
+  <div class="justify-center text-center mt-[70px]">
+    <!-- Title -->
+    <div class="font-bold text-[35px]">
+      <h1>Menu</h1>
+    </div>
+
+    <!-- Description-->
+    <div class="font-semibold text-[17px]">
+      <h2>View the full menu.</h2>
+    </div>
+  </div>
+
+  <!-- Menu products -->
+  <div class="flex justify-center items-center mt-[30px]">
+    <div class="grid grid-cols-2 gap-x-[15px] gap-y-[60px]">
+      <MenuProduct
+        to="/products/sausage-roll"
+        name="Sausage Roll"
+        image="/products/sausage-roll.avif"
+      />
+
+      <MenuProduct
+        to="/products/sausage-roll"
+        name="Donut"
+        image="/products/donut.webp"
+      />
+
+    </div>
+  </div>
 </template>
