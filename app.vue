673b5720330500b565a8956981c6405a6c386bf1
Shayden seymour <shaydenseymour@gmail.com>
2025-05-07 18:37:26 +1200

    App.vue updated

--- app.vue
+++ app.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+      <TabBar />
+      <LoadingScreen />
+
+      <div>
+        
+      </div>
+    </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+import { WebSocket } from "~/compostables/WebSocket"
+import { onMounted } from "vue"
+
+onMounted(() => {
+  WebSocket.connect();
+});
+
+// <ModelsMore />
+</script>
