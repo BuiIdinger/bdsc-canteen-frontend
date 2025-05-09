ec0f68522aeaa4f642821b1341fa9e96346263ee
Shayden seymour <shaydenseymour@gmail.com>
2025-05-09 20:09:07 +1200

    Legal icon

---
+++ components/icons/Legal.vue
@@ -0,0 +1,56 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 331.8 469.18">
+    <!-- Generator: Adobe Illustrator 29.5.1, SVG Export Plug-In . SVG Version: 2.1.0 Build 141)  -->
+    <g id="Layer_1">
+      <rect class="st4" width="294.19" height="383.23" rx="10.82" ry="10.82"/>
+      <circle class="st1" cx="19.02" cy="80.2" r="6.6"/>
+      <circle class="st1" cx="19.02" cy="101.89" r="6.6"/>
+      <rect class="st0" x="38.47" y="86.66" width="184.19" height="11.52" rx="2.71" ry="2.71"/>
+      <rect class="st0" x="38.47" y="101.88" width="135.35" height="11.52" rx="2.71" ry="2.71"/>
+      <rect class="st0" x="38.47" y="26.49" width="184.19" height="11.52" rx="2.71" ry="2.71"/>
+      <rect class="st0" x="38.47" y="41.72" width="135.35" height="11.52" rx="2.71" ry="2.71"/>
+      <rect class="st0" x="38.47" y="146.82" width="206.06" height="11.52" rx="2.71" ry="2.71"/>
+      <rect class="st0" x="38.47" y="162.04" width="206.06" height="11.52" rx="2.71" ry="2.71"/>
+      <rect class="st0" x="38.47" y="206.98" width="141.16" height="11.52" rx="2.71" ry="2.71"/>
+      <rect class="st0" x="38.47" y="222.2" width="197.29" height="11.52" rx="2.71" ry="2.71"/>
+      <rect class="st0" x="38.47" y="267.11" width="166.71" height="11.52" rx="2.71" ry="2.71"/>
+      <rect class="st0" x="38.47" y="282.33" width="98.65" height="11.52" rx="2.71" ry="2.71"/>
+      <rect class="st0" x="38.47" y="326.26" width="166.71" height="11.52" rx="2.71" ry="2.71"/>
+      <rect class="st0" x="38.47" y="341.49" width="98.65" height="11.52" rx="2.71" ry="2.71"/>
+      <circle class="st1" cx="19.13" cy="281.33" r="6.6"/>
+      <circle class="st1" cx="19.13" cy="303.03" r="6.6"/>
+      <ellipse class="st4" cx="160.45" cy="452.54" rx="129.48" ry="16.65"/>
+    </g>
+    <g id="Layer_2">
+      <circle class="st2" cx="281.8" cy="369.91" r="50"/>
+      <line class="st3" x1="281.8" y1="336.3" x2="281.8" y2="403.52"/>
+      <path class="st3" d="M297.08,348.53h-22.91c-5.91,0-10.69,4.79-10.69,10.69s4.79,10.69,10.69,10.69h15.28c5.91,0,10.69,4.79,10.69,10.69s-4.79,10.69-10.69,10.69h-25.97"/>
+    </g>
+  </svg>
+</template>
+
+<style>
+.st0 {
+  fill: #999b9e;
+}
+
+.st1 {
+  fill: #fff;
+}
+
+.st2 {
+  fill: #e8b121;
+}
+
+.st3 {
+  fill: none;
+  stroke: #f1f2f2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-width: 7px;
+}
+
+.st4 {
+  fill: #dcddde;
+}
+</style>
