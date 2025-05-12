332902be8403aa9ac1a54ebbfd1585d2445a9744
Shayden seymour <shaydenseymour@gmail.com>
2025-05-12 12:32:53 +1200

    Remove devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image']
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: false },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image"
+  ],
 })
