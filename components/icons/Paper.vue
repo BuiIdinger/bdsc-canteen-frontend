a6d1e1296ec9cc3388951268604c7bf59dba377c
Shayden seymour <shaydenseymour@gmail.com>
2025-05-12 12:33:07 +1200

    Paper vector

---
+++ components/icons/Paper.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" baseProfile="tiny" version="1.2" viewBox="0 0 359.65 469.18">
+    <!-- Generator: Adobe Illustrator 29.5.1, SVG Export Plug-In . SVG Version: 2.1.0 Build 141)  -->
+    <g id="Layer_1">
+      <rect width="294.19" height="383.23" rx="10.82" ry="10.82" fill="#dcddde"/>
+      <circle cx="19.02" cy="80.2" r="6.6" fill="#fff"/>
+      <circle cx="19.02" cy="101.89" r="6.6" fill="#fff"/>
+      <rect x="38.47" y="86.66" width="184.19" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <rect x="38.47" y="101.88" width="135.35" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <rect x="38.47" y="26.49" width="184.19" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <rect x="38.47" y="41.72" width="135.35" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <rect x="38.47" y="146.82" width="206.06" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <rect x="38.47" y="162.04" width="206.06" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <rect x="38.47" y="206.98" width="141.16" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <rect x="38.47" y="222.2" width="197.29" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <rect x="38.47" y="267.11" width="166.71" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <rect x="38.47" y="282.33" width="98.65" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <rect x="38.47" y="326.26" width="166.71" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <rect x="38.47" y="341.49" width="98.65" height="11.52" rx="2.71" ry="2.71" fill="#999b9e"/>
+      <circle cx="19.13" cy="281.33" r="6.6" fill="#fff"/>
+      <circle cx="19.13" cy="303.03" r="6.6" fill="#fff"/>
+      <ellipse cx="160.45" cy="452.54" rx="129.48" ry="16.65" fill="#dcddde"/>
+    </g>
+    <g id="Layer_2" display="none">
+      <circle cx="281.8" cy="369.91" r="50" fill="#e8b121"/>
+      <line x1="281.8" y1="336.3" x2="281.8" y2="403.52" fill="none" stroke="#f1f2f2" stroke-linecap="round" stroke-linejoin="round" stroke-width="7"/>
+      <path d="M297.08,348.53h-22.91c-5.91,0-10.69,4.79-10.69,10.69s4.79,10.69,10.69,10.69h15.28c5.91,0,10.69,4.79,10.69,10.69s-4.79,10.69-10.69,10.69h-25.97" fill="none" stroke="#f1f2f2" stroke-linecap="round" stroke-linejoin="round" stroke-width="7"/>
+    </g>
+    <g id="Layer_3" display="none">
+      <path d="M327.25,239.14h23.48c3.31,0,5.99,2.68,5.99,5.99v20.96h-35.46v-20.96c0-3.31,2.68-5.99,5.99-5.99Z" transform="translate(153.42 -127.24) rotate(27.27)" fill="#ee2636"/>
+      <rect x="312.6" y="263.99" width="35.46" height="10.85" transform="translate(160.15 -121.4) rotate(27.27)" fill="#d8d9da"/>
+      <rect x="279.38" y="266.78" width="35.46" height="134.15" transform="translate(185.98 -99.02) rotate(27.27)" fill="#eca33a"/>
+      <path d="M250.24,412.08c.13-8.91.25-17.82.38-26.72,10.51,5.42,21.02,10.83,31.52,16.25-7.18,5.27-14.36,10.54-21.54,15.81-3.45-1.78-6.91-3.56-10.36-5.34Z" fill="#fecc94"/>
+      <path d="M250.24,412.08c-.05,3.67-.1,7.34-.16,11.01.02.08.15.53.62.78.47.25.91.11.99.09,2.97-2.18,5.94-4.36,8.91-6.54-3.45-1.78-6.91-3.56-10.36-5.34Z" fill="#231f20"/>
+    </g>
+  </svg>
+</template>
