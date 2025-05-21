705a0603750b125c32bc4d19edf62b852d1afe4a
Shayden seymour <shaydenseymour@gmail.com>
2025-05-21 20:30:07 +1200

    Form error compontent

---
+++ components/FormError.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex gap-x-[8px] fill-red-700 text-red-700 items-center">
+    <!-- Icon -->
+    <div>
+      <IconsWarning class="h-[20px] w-[20px]" />
+    </div>
+
+    <!-- Text -->
+    <div class="font-medium">
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    required: true,
+    default: "NULL"
+  },
+});
+</script>
