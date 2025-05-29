cd9dc1ddcc822ffd8cffbe4fcb41c387a14cae6d
Shayden seymour <shaydenseymour@gmail.com>
2025-05-29 13:03:55 +1200

    Add first name and last name to userdata

--- compostables/UserData.ts
+++ compostables/UserData.ts
@@ -3,4 +3,6 @@ import { ref } from "vue"
 export namespace UserData {
   export const id = ref<string>("NULL");
   export const email = ref<string>("NULL");
+  export const firstName = ref<string>("NULL");
+  export const lastName = ref<string>("NULL");
 }
